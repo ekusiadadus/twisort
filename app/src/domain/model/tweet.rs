b70fb13e745a30eefbd23854d3e072e5c3f67e81
ekusiadadus <ekusiadadus@gmail.com>
2022-11-15 01:04:09 +0900

    refactor: :fire: serde unserialize tweets

--- app/src/domain/model/tweet.rs
+++ app/src/domain/model/tweet.rs
@@ -3,38 +3,39 @@ use serde::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
+
 pub struct Tweet {
     pub id: TweetID,
     pub text: String,
     pub author_id: String,
     pub created_at: String,
-    pub entities: String,
-    pub geo: Option<String>,
+    pub entities: Option<serde_json::Value>,
+    pub geo: Option<serde_json::Value>,
     pub in_reply_to_user_id: Option<String>,
-    pub lang: String,
+    pub lang: Option<String>,
     pub possibly_sensitive: Option<bool>,
-    pub referenced_tweets: Option<String>,
-    pub source: String,
-    pub withheld: Option<String>,
+    pub referenced_tweets: Option<Vec<serde_json::Value>>,
+    pub source: Option<String>,
+    pub withheld: Option<serde_json::Value>,
 }
 
 impl Tweet {
     pub fn new(
-        id: String,
+        id: TweetID,
         text: String,
         author_id: String,
         created_at: String,
-        entities: String,
-        geo: Option<String>,
+        entities: Option<serde_json::Value>,
+        geo: Option<serde_json::Value>,
         in_reply_to_user_id: Option<String>,
-        lang: String,
+        lang: Option<String>,
         possibly_sensitive: Option<bool>,
-        referenced_tweets: Option<String>,
-        source: String,
-        withheld: Option<String>,
+        referenced_tweets: Option<Vec<serde_json::Value>>,
+        source: Option<String>,
+        withheld: Option<serde_json::Value>,
     ) -> Self {
-        Self {
-            id: TweetID(id),
+        Tweet {
+            id,
             text,
             author_id,
             created_at,
--- app/src/repository/tweet_repo.rs
+++ app/src/repository/tweet_repo.rs
@@ -49,39 +49,63 @@ pub struct TweetRecord {
 
 impl TweetRecord {
     pub fn to_model(self) -> Result<Tweet> {
-        let tweet = Tweet {
-            id: TweetID(self.id),
-            text: self.text,
-            author_id: self.author_id,
-            created_at: self.created_at,
-            entities: self.entities,
-            geo: self.geo,
-            in_reply_to_user_id: self.in_reply_to_user_id,
-            lang: self.lang,
-            possibly_sensitive: self.possibly_sensitive,
-            referenced_tweets: self.referenced_tweets,
-            source: self.source,
-            withheld: self.withheld,
+        let entities: Option<serde_json::Value> = serde_json::from_str(&self.entities).unwrap();
+        let geo: Option<serde_json::Value> = match self.geo {
+            Some(geo) => Some(serde_json::from_str(&geo).unwrap()),
+            None => None,
         };
-        Ok(tweet)
+        let referenced_tweets: Option<Vec<serde_json::Value>> = match self.referenced_tweets {
+            Some(referenced_tweets) => Some(serde_json::from_str(&referenced_tweets).unwrap()),
+            None => None,
+        };
+        let withheld: Option<serde_json::Value> = match self.withheld {
+            Some(withheld) => Some(serde_json::from_str(&withheld).unwrap()),
+            None => None,
+        };
+        Ok(Tweet::new(
+            TweetID(self.id),
+            self.text,
+            self.author_id,
+            self.created_at,
+            entities,
+            geo,
+            self.in_reply_to_user_id,
+            Some(self.lang),
+            self.possibly_sensitive,
+            referenced_tweets,
+            Some(self.source),
+            withheld,
+        ))
     }
 
     pub fn from_model(tweet: Tweet) -> Result<Self> {
-        let tweet_record = TweetRecord {
-            id: tweet.id.0.clone(),
-            text: tweet.text.clone(),
-            author_id: tweet.author_id.clone(),
-            created_at: tweet.created_at.clone(),
-            entities: tweet.entities.clone(),
-            geo: tweet.geo.clone(),
-            in_reply_to_user_id: tweet.in_reply_to_user_id.clone(),
-            lang: tweet.lang.clone(),
-            possibly_sensitive: tweet.possibly_sensitive.clone(),
-            referenced_tweets: tweet.referenced_tweets.clone(),
-            source: tweet.source.clone(),
-            withheld: tweet.withheld.clone(),
+        let entities = serde_json::to_string(&tweet.entities).unwrap();
+        let geo = match tweet.geo {
+            Some(geo) => Some(serde_json::to_string(&geo).unwrap()),
+            None => None,
         };
-        Ok(tweet_record)
+        let referenced_tweets = match tweet.referenced_tweets {
+            Some(referenced_tweets) => Some(serde_json::to_string(&referenced_tweets).unwrap()),
+            None => None,
+        };
+        let withheld = match tweet.withheld {
+            Some(withheld) => Some(serde_json::to_string(&withheld).unwrap()),
+            None => None,
+        };
+        Ok(TweetRecord {
+            id: tweet.id.0.clone(),
+            text: tweet.text,
+            author_id: tweet.author_id,
+            created_at: tweet.created_at,
+            entities,
+            geo,
+            in_reply_to_user_id: tweet.in_reply_to_user_id,
+            lang: tweet.lang.unwrap(),
+            possibly_sensitive: tweet.possibly_sensitive,
+            referenced_tweets,
+            source: tweet.source.unwrap(),
+            withheld,
+        })
     }
 }
 
@@ -105,14 +129,15 @@ impl TweetRepository {
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize, Serialize, Default)]
 #[serde(rename_all = "camelCase")]
+
 pub struct TweetResponse {
-    pub data: Vec<Tweet>,
-    pub meta: TweetResponseMeta,
+    data: Vec<Tweet>,
+    meta: Option<TweetResponseMeta>,
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize, Serialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct TweetResponseMeta {
     pub newest_id: String,
@@ -163,14 +188,16 @@ impl ITweetRepository for TweetRepository {
             reqwest::header::CONTENT_TYPE,
             "application/json".parse().unwrap(),
         );
-        let response = self
-            .http_client
-            .get(&uri, Some(headers))
-            .await?
-            .text()
-            .await?;
-        let tweets = serde_json::from_str::<TweetResponse>(&response)
-            .map_err(|e| ServiceError::new(TweetRepoError::HttpClientError, e))?;
+        let response = self.http_client.get(&uri, Some(headers)).await.unwrap();
+
+        let body = response.text().await.unwrap();
+
+        print!("{}", body);
+
+        let tweets = serde_json::from_str::<TweetResponse>(&body).unwrap();
+
+        print!("{:?}", tweets.data);
+
         Ok(tweets.data)
     }
 
@@ -195,3 +222,291 @@ impl ITweetRepository for TweetRepository {
         Ok(())
     }
 }
+
+// TweetResponse
+// {
+//   "data": [
+//     {
+//       "possibly_sensitive": false,
+//       "referenced_tweets": [
+//         {
+//           "type": "quoted",
+//           "id": "1592094998769434627"
+//         }
+//       ],
+//       "id": "1592104440001359873",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 25,
+//             "end": 48,
+//             "url": "https://t.co/fYEKtksj9O",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1592104440001359873/photo/1",
+//             "display_url": "pic.twitter.com/fYEKtksj9O",
+//             "media_key": "3_1592104369092460544"
+//           },
+//           {
+//             "start": 49,
+//             "end": 72,
+//             "url": "https://t.co/luAE3sLVVu",
+//             "expanded_url": "https://twitter.com/Scenify_jp/status/1592094998769434627",
+//             "display_url": "twitter.com/Scenify_jp/sta…"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-14T10:37:33.000Z",
+//       "edit_history_tweet_ids": [
+//         "1592104440001359873"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter Web App",
+//       "lang": "ja",
+//       "text": "完全に、crypkoで草\nサービスとしてはよさげ https://t.co/fYEKtksj9O https://t.co/luAE3sLVVu"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1592094492223737856",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 14,
+//             "end": 37,
+//             "url": "https://t.co/jcNCxgn7bG",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1592094492223737856/photo/1",
+//             "display_url": "pic.twitter.com/jcNCxgn7bG",
+//             "media_key": "3_1592094450511097857"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-14T09:58:01.000Z",
+//       "edit_history_tweet_ids": [
+//         "1592094492223737856"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter Web App",
+//       "lang": "ja",
+//       "text": "これ、面白すぎるだろ（笑） https://t.co/jcNCxgn7bG"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1592078474898214912",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 18,
+//             "end": 41,
+//             "url": "https://t.co/zcYrCUoQwI",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1592078474898214912/photo/1",
+//             "display_url": "pic.twitter.com/zcYrCUoQwI",
+//             "media_key": "3_1592078471530164225"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-14T08:54:22.000Z",
+//       "edit_history_tweet_ids": [
+//         "1592078474898214912"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter for Android",
+//       "lang": "ja",
+//       "text": "うまうま\n\n油淋鶏が、一番好きかも https://t.co/zcYrCUoQwI"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1591867817766576128",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 42,
+//             "end": 65,
+//             "url": "https://t.co/zBi4OawaL9",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1591867817766576128/photo/1",
+//             "display_url": "pic.twitter.com/zBi4OawaL9",
+//             "media_key": "3_1591867811827441665"
+//           },
+//           {
+//             "start": 42,
+//             "end": 65,
+//             "url": "https://t.co/zBi4OawaL9",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1591867817766576128/photo/1",
+//             "display_url": "pic.twitter.com/zBi4OawaL9",
+//             "media_key": "3_1591867815220645890"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-13T18:57:17.000Z",
+//       "edit_history_tweet_ids": [
+//         "1591867817766576128"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter for Android",
+//       "lang": "ja",
+//       "text": "鹿肉のパイ生地包は、美味しかったんだけど\n\nサラダについてくるプリンが、苦手だった https://t.co/zBi4OawaL9"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1591853814529003521",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 0,
+//             "end": 23,
+//             "url": "https://t.co/29rdnXshA3",
+//             "expanded_url": "https://gist.github.com/ekusiadadus/e267f700ff76847ba22cd280b0940882",
+//             "display_url": "gist.github.com/ekusiadadus/e2…",
+//             "images": [
+//               {
+//                 "url": "https://pbs.twimg.com/news_img/1591853879817560066/1uWb-I1f?format=png&name=orig",
+//                 "width": 1280,
+//                 "height": 640
+//               },
+//               {
+//                 "url": "https://pbs.twimg.com/news_img/1591853879817560066/1uWb-I1f?format=png&name=150x150",
+//                 "width": 150,
+//                 "height": 150
+//               }
+//             ],
+//             "status": 200,
+//             "title": "get_tweets.rs",
+//             "description": "get_tweets.rs. GitHub Gist: instantly share code, notes, and snippets.",
+//             "unwound_url": "https://gist.github.com/ekusiadadus/e267f700ff76847ba22cd280b0940882"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-13T18:01:39.000Z",
+//       "edit_history_tweet_ids": [
+//         "1591853814529003521"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter Web App",
+//       "lang": "zxx",
+//       "text": "https://t.co/29rdnXshA3"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1591840639636865026",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 82,
+//             "end": 105,
+//             "url": "https://t.co/XcTHXGMXKc",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1591840639636865026/photo/1",
+//             "display_url": "pic.twitter.com/XcTHXGMXKc",
+//             "media_key": "3_1591840607982350337"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-13T17:09:18.000Z",
+//       "edit_history_tweet_ids": [
+//         "1591840639636865026"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter Web App",
+//       "lang": "ja",
+//       "text": "Rust Diesel Sqlite に multi thread で書き込もうとしている人を、この世に他に一人だけ見つけ出した。\n\nまじで、何やっているんだろう https://t.co/XcTHXGMXKc"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1591674286527610882",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 25,
+//             "end": 48,
+//             "url": "https://t.co/PLoz3sOoe4",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1591674286527610882/photo/1",
+//             "display_url": "pic.twitter.com/PLoz3sOoe4",
+//             "media_key": "3_1591674226905186307"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-13T06:08:16.000Z",
+//       "edit_history_tweet_ids": [
+//         "1591674286527610882"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter Web App",
+//       "lang": "ja",
+//       "text": "Pythonを書いていなさすぎるという強い危機感 https://t.co/PLoz3sOoe4"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1591665072564473856",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 115,
+//             "end": 138,
+//             "url": "https://t.co/PhUTV2Wfhg",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1591665072564473856/photo/1",
+//             "display_url": "pic.twitter.com/PhUTV2Wfhg",
+//             "media_key": "3_1591665055640080384"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-13T05:31:39.000Z",
+//       "edit_history_tweet_ids": [
+//         "1591665072564473856"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter Web App",
+//       "lang": "ja",
+//       "text": "切れそう\n\nexpected &amp;mut SqliteConnection, found &amp;PooledConnection&lt;ConnectionManager&lt;SqliteConnection&gt; https://t.co/PhUTV2Wfhg"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1591562335638163456",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 9,
+//             "end": 32,
+//             "url": "https://t.co/4wPIr2sniI",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1591562335638163456/photo/1",
+//             "display_url": "pic.twitter.com/4wPIr2sniI",
+//             "media_key": "3_1591562331414462464"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-12T22:43:25.000Z",
+//       "edit_history_tweet_ids": [
+//         "1591562335638163456"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter for Android",
+//       "lang": "ja",
+//       "text": "朝ですぜ\n\n寝る https://t.co/4wPIr2sniI"
+//     },
+//     {
+//       "possibly_sensitive": false,
+//       "id": "1591560289069432832",
+//       "entities": {
+//         "urls": [
+//           {
+//             "start": 4,
+//             "end": 27,
+//             "url": "https://t.co/VCpgo9f8U7",
+//             "expanded_url": "https://twitter.com/ekusiadadus/status/1591560289069432832/photo/1",
+//             "display_url": "pic.twitter.com/VCpgo9f8U7",
+//             "media_key": "3_1591560285512663040"
+//           }
+//         ]
+//       },
+//       "created_at": "2022-11-12T22:35:17.000Z",
+//       "edit_history_tweet_ids": [
+//         "1591560289069432832"
+//       ],
+//       "author_id": "1299285051436273664",
+//       "source": "Twitter for Android",
+//       "lang": "ja",
+//       "text": "綺麗や https://t.co/VCpgo9f8U7"
+//     }
+//   ],
+//   "meta": {
+//     "newest_id": "1592104440001359873",
+//     "oldest_id": "1591560289069432832",
+//     "result_count": 10,
+//     "next_token": "b26v89c19zqg8o3fpzhjn2uq157wnmzphblk5a42ls1a5"
+//   }
+// }
